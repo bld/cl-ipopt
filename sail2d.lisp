aee3d01f725710731716903b1c7122b4611bf1d6
Ben Diedrich <bldiedrich@gmail.com>
2016-08-31 08:54:39 -0500

    Continued work on sail 2d example. Still not converging. Exports traj to data file for plotting.

--- sail2d.lisp
+++ sail2d.lisp
@@ -9,7 +9,7 @@
     (setf (mem-aref grad_f :double i) 0d0))
   (setf (mem-aref grad_f :double (1- n)) 1d0)
   1)
-  
+
 (defclass sail-data ()
   ((mu :initarg :mu)
    (lightness :initarg :lightness)
@@ -48,40 +48,30 @@
        for xfi = (second (car (last res)))
        append res)))
 
-(defcallback sail2d_g :int ((n :int) (x :pointer) (new_x :int) (m :int) (g :pointer))
+(defun sail2d-g (x)
   (let ((rf 1.5d0)
 	(vrf 0d0)
 	(vtf 0d0)
-	(xv (make-array n)))
-    (dotimes (i n)
-      (setf (aref xv i) (mem-aref x :double i)))
-    (let* ((res (sail2d-integrate xv))
-	   (xf (second (car (last res)))))
-      (setf (mem-aref g :double 0) (- rf (aref xf 0))
-	    (mem-aref g :double 1) (- vrf (aref xf 2))
-	    (mem-aref g :double 2) (- vtf (aref xf 3)))))
+	(xf (second (car (last (sail2d-integrate x))))))
+    (vector (- rf (aref xf 0))
+	    (- vrf (aref xf 2))
+	    (- vtf (aref xf 3)))))
+
+(defcallback sail2d_g :int ((n :int) (x :pointer) (new_x :int) (m :int) (g :pointer))
+  (let ((gv (sail2d-g (from-foreign-vector x :double n))))
+    (set-foreign-vector gv g :double m))
   1)
 
 (defcallback sail2d_jac_g :int ((n :int) (x :pointer) (new_x :int) (m :int) (nele_jac :int) (irow :pointer) (jcol :pointer) (values :pointer))
   (assert (null-pointer-p values)) ; always estimate Jacobian
   ;; Return Jacobian structure: m x n: 3 x n
-  (let* ((structure (jacobian-structure m n))
-	 (l (length structure)))
-    (assert (= l nele_jac))
-    (loop for k below l
-       for (i j) in structure
-       do (setf (mem-aref irow :int k) i (mem-aref jcol :int k) j)))
+  (set-structure (jacobian-structure m n) irow jcol)
   1)
 
 (defcallback sail2d_h :int ((n :int) (x :pointer) (new_x :int) (obj_factor :double) (m :int) (lmb :pointer) (new_lmb :int) (nele_hess :int) (irow :pointer) (jcol :pointer) (values :pointer))
   (if (null-pointer-p values)
       ;; Return Hessian structure: n x n
-      (let* ((structure (hessian-structure n))
-	     (l (length structure)))
-	(assert (= l nele_hess))
-	(loop for k below l
-	   for (row col) in structure
-	   do (setf (mem-aref irow :int k) row (mem-aref jcol :int k) col)))
+      (set-structure (hessian-structure n) irow jcol)
       ;; Otherwise return Hessian
       (dotimes (i nele_hess)
 	(setf (mem-aref values :double i) 0d0)))
@@ -118,9 +108,9 @@
       (let ((nlp (createipoptproblem n x_l x_u m g_l g_u (length (jacobian-structure m n)) (length (hessian-structure n)) 0 (callback sail2d_f) (callback sail2d_g) (callback sail2d_grad_f) (callback sail2d_jac_g) (callback sail2d_h))))
 
 	;; Set options
-	(addipoptnumoption nlp "tol" 1d-9)
-	(addipoptintoption nlp "max_iter" 100)
-	(addipoptstroption nlp "mu_strategy" "adaptive")
+	;;(addipoptnumoption nlp "tol" 1d-9)
+	(addipoptintoption nlp "max_iter" 1000)
+	;;(addipoptstroption nlp "mu_strategy" "adaptive")
 	;;(addipoptstroption nlp "hessian_approximation" "limited-memory")
 	(addipoptstroption nlp "jacobian_approximation" "finite-difference-values")
 	
@@ -134,4 +124,15 @@
 
 	;; Solve problem
 	(ipoptsolve nlp x (null-pointer) obj (null-pointer) mult_x_l mult_x_u (null-pointer))
+	(let* ((xv (from-foreign-vector x :double n))
+	       (f (mem-ref obj :double))
+	       (g (sail2d-g xv))
+	       (traj (sail2d-integrate xv)))
+	  (export-trajectory traj "sail2d_traj.dat")
+	  (values xv f g))
 	))))
+
+(defun export-trajectory (traj file)
+  (with-open-file (s file :direction :output :if-exists :supersede)
+    (loop for (tm x) in traj
+       do (format s "~a ~a ~a ~a ~a~%" tm (aref x 0) (aref x 1) (aref x 2) (aref x 3)))))
